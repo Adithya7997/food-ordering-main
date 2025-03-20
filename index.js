631cecda6f4539e4d4d616178fd34ac9608d7e18
Adithya7997 <inavoluadithya893@gmail.com>
2025-03-20 19:47:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+window.onscroll = () =>{
+    if (window.scrollY > 60) {
+        document.querySelector('#scroll-top').classList.add('active');
+        
+    } else {
+        document.querySelector('#scroll-top').classList.remove('active');
+    }
+}
+
+
+function fadeOut(){
+    setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
 
